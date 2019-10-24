4cdbc5e18134fa2f938bffde2a531028da1ac55b
som1else <samnang@protonmail.com>
2019-10-24 02:00:13 -0400

    add

---
+++ system/.scripts/disablegpu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+modprobe -r nvidia_drm
+modprobe -r nvidia_uvm
+modprobe -r nvidia_modeset
+modprobe -r nvidia
+
+# change NVIDIA card power control
+echo -n auto > /sys/bus/pci/devices/0000\:01\:00.0/power/control
+sleep 1
+# change pcie power control
+echo -n auto > /sys/bus/pci/devices/0000\:00\:01.0/power/control
+sleep 1
+
+# lock system from loading nvidia module
+mv /etc/modprobe.d/disable-nvidia.conf.disable /etc/modprobe.d/disable-nvidia.conf
+
+
